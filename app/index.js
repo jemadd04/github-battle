cb42bfd5dcdd0b41e1ab83682892c6f9d71b1432
James Maddox <james.maddox07@gmail.com>
2020-04-06 16:31:57 -0400

    added css styling

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Popular from './components/Popular';
 
 // This defines the component
 class App extends React.Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    );
   }
 }
 
